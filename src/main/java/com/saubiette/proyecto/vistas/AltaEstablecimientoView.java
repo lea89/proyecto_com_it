f0e933d0609c5942f2433ea96e108bcf3b6da5a6
Pablo Fernandez <pmfernan@gmail.com>
2021-04-04 15:25:21 -0300

    Modificaciones AltaEstablecimientoView

--- src/main/java/com/saubiette/proyecto/vistas/AltaEstablecimientoView.java
+++ src/main/java/com/saubiette/proyecto/vistas/AltaEstablecimientoView.java
@@ -1,6 +1,5 @@
 package com.saubiette.proyecto.vistas;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +15,9 @@ import com.vaadin.flow.component.combobox.ComboBox;
 import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.Span;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.notification.Notification.Position;
+import com.vaadin.flow.component.notification.NotificationVariant;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.data.binder.Binder;
@@ -31,19 +33,16 @@ public class AltaEstablecimientoView extends VerticalLayout {
 
 	private static final long serialVersionUID = 1L;
 
-	@Autowired
 	EstablecimientoController establecimientoController;
-
-	@Autowired
 	TipoEstController tipoEstController;
 
-	TextField txtNombre;
-	TextField txtDireccion;
-	Span titulo;
-	Span error;
-	Div div;
+	TextField direccion = new TextField("Nombre: ");
+	TextField nombre = new TextField("Dirección: ");
+	Span titulo = new Span("Alta de establecimiento");
+	Span error = new Span();
+	Div div = new Div();
 
-	ComboBox<TipoEstablecimiento> comboTipo;
+	ComboBox<TipoEstablecimiento> tipo = new ComboBox<TipoEstablecimiento>("Tipo:");
 
 	List<Establecimiento> listEstablecimientos;
 	List<TipoEstablecimiento> listTipoEst;
@@ -52,92 +51,82 @@ public class AltaEstablecimientoView extends VerticalLayout {
 
 	Binder<Establecimiento> binder;
 
+	private Establecimiento establecimiento;
+
+	@Autowired
 	public AltaEstablecimientoView(EstablecimientoController establecimientoController,
 			TipoEstController tipoEstController) {
 
 		this.establecimientoController = establecimientoController;
 		this.tipoEstController = tipoEstController;
 
-		Menu menu = new Menu();
+		init();
+		configBinder();
 
-		setElements();
-		setValueElements();
-		setListeners();
-
-		div = new Div();
-
-		div.addClassName("contenedorForm");
-		div.add(titulo);
-		div.add(txtNombre);
-		div.add(txtDireccion);
-		div.add(comboTipo);
-		div.add(btnCrear);
-		div.add(error);
-
-		add(menu, div);
 	}
 
-	public void setElements() {
-		titulo = new Span("Alta de establecimiento");
-		titulo.addClassName("titulo");
+	public void init() {
+		establecimiento = new Establecimiento();
 
-		error = new Span();
+		tipo = crearComboTipos();
+
 		error.addClassName("error");
-		txtNombre = new TextField("Nombre:");
-		txtDireccion = new TextField("Direccion:");
+		titulo.addClassName("titulo");
+		div.addClassName("contenedorForm");
 
-		listEstablecimientos = new ArrayList<Establecimiento>();
-
-		listTipoEst = new ArrayList<TipoEstablecimiento>();
-
-		comboTipo = new ComboBox<TipoEstablecimiento>();
 		btnCrear = new Button("Crear establecimiento");
 
-		binder = new Binder<>(Establecimiento.class);
+		btnCrear.addClickListener(listener -> guardar());
 
-		binder.forField(txtNombre).asRequired().bind(Establecimiento::getNombre, Establecimiento::setNombre);
-		binder.forField(txtDireccion).asRequired().bind(Establecimiento::getDireccion, Establecimiento::setDireccion);
-		binder.forField(comboTipo).asRequired().bind(Establecimiento::getTipo, Establecimiento::setTipo);
+		div.add(titulo);
+		div.add(nombre);
+		div.add(direccion);
+		div.add(tipo);
+		div.add(btnCrear);
+		div.add(error);
+		Menu menu = new Menu();
+		add(menu, div);
 
 	}
 
-	public void setValueElements() {
+	private void configBinder() {
+		binder = new Binder<>(Establecimiento.class);
 
-		// establecimientoController.traerEstablecimientos().forEach(listEstablecimientos::add);
+		binder.bindInstanceFields(this);
 
-		if (tipoEstController.traerTipos() != null) {
-			tipoEstController.traerTipos().forEach(x -> {
-				listTipoEst.add(x);
-			});
-		}
+		binder.forField(nombre).asRequired("El campo Nombre es requerido").bind("nombre");
+		binder.forField(direccion).asRequired("El campo Direccion es requerido").bind("direccion");
+		binder.forField(tipo).asRequired("El campo Tipo es requerido").bind("tipo");
 
+		binder.setBean(establecimiento);
+
+	}
+
+	private ComboBox<TipoEstablecimiento> crearComboTipos() {
+		List<TipoEstablecimiento> listTipoEst = (List<TipoEstablecimiento>) tipoEstController.traerTipos();
+		ComboBox<TipoEstablecimiento> comboTipo = new ComboBox<TipoEstablecimiento>();
 		comboTipo.setLabel("Tipo de establecimiento");
 		comboTipo.setItemLabelGenerator(TipoEstablecimiento::getNombre);
 		comboTipo.setItems(listTipoEst);
+		return comboTipo;
 	}
 
-	public void setListeners() {
-		btnCrear.addClickListener(e -> {
+	public void guardar() {
 
-			error.setText("");
+		error.setText("");
+		try {
+			binder.writeBean(establecimiento);
+			establecimientoController.crearEstablecimiento(establecimiento);
+			Notification.show("El registro fue dado de alta satisfactoriamente", 3000, Position.TOP_CENTER)
+					.addThemeVariants(NotificationVariant.LUMO_SUCCESS);
 
-			Establecimiento est = new Establecimiento();
+		} catch (ValidationException ex) {
+			Notification.show("El registro no pudo guardarse", 3000, Position.TOP_CENTER)
+					.addThemeVariants(NotificationVariant.LUMO_ERROR);
+			ex.printStackTrace();
+			error.setText(ex.getMessage());
+		}
 
-			binder.validate();
-			if (binder.isValid()) {
-				try {
-					binder.writeBean(est);
-					establecimientoController.crearEstablecimiento(est);
-
-				} catch (ValidationException ex) {
-					error.setText(ex.getMessage());
-				}
-
-			} else {
-				System.out.println(binder.validate().getBeanValidationErrors());
-				error.setText("Por favor verifique los campos e intente nuevamente.");
-			}
-
-		});
 	}
+
 }
---
+++ src/main/resources/data.sql
@@ -0,0 +1,1 @@
+INSERT INTO TIPO_ESTABLECIMIENTO(nombre) VALUES ('tipo 1'),('tipo 2'); 
